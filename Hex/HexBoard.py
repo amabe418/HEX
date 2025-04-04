10b305a9814f8067cd4e5b1869aa48e3e8df614b
amabe418 <valienteamalia418@gmail.com>
2025-04-04 18:22:03 -0400

    first(and maybe last) version of place_piece method

--- Hex/HexBoard.py
+++ Hex/HexBoard.py
@@ -1,17 +1,21 @@
+import numpy
 
 class HexBoard:
     def __init__(self, size: int):
         self.size = size  # Tamaño N del tablero (NxN)
-        self.board = [[0 for _ in range(size)] for _ in range(size)]  # Matriz NxN (0=vacío, 1=Jugador1, 2=Jugador2)
+        self.board = numpy.zeros((size, size), dtype = int)  # Matriz NxN (0=vacío, 1=Jugador1, 2=Jugador2)
+         
 
     def clone(self) -> "HexBoard":
         """Devuelve una copia del tablero actual"""
         pass  # Aquí luego puedes implementar la clonación del tablero
 
     def place_piece(self, row: int, col: int, player_id: int) -> bool:
-        """Coloca una ficha si la casilla está vacía."""
-        pass
-
+        if  self.board[row][col] is 0:
+            self.board[row][col] = player_id
+            return True
+        return False
+        
     def get_possible_moves(self) -> list:
         """Devuelve todas las casillas vacías como tuplas (fila, columna)."""
         pass
